6dd831f88e56f529ea0a4cc05d56197f53f1a431
urielsmp128 <urielsmp128@outlook.com>
2024-06-01 20:46:24 -0600

    Implementing header and footer components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
+import FooterComponent from './components/FooterComponent'
+import HeaderComponent from './components/HeaderComponent'
 import ListEmployeeComponent from './components/ListEmployeeComponent'
 
 function App() {
 
   return (
     <>
+      <HeaderComponent/>
       <ListEmployeeComponent/>
+      <FooterComponent/>
     </>
   )
 }
