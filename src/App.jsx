c90ea0566285b73b72f53cb4fcb7eb3e8b1df904
urielsmp128 <urielsmp128@outlook.com>
2024-06-12 17:09:23 -0600

    Initializing Department Component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import EmployeeComponent from './components/EmployeeComponent'
 import FooterComponent from './components/FooterComponent'
 import HeaderComponent from './components/HeaderComponent'
+import ListDepartmentComponent from './components/ListDepartmentComponent'
 import ListEmployeeComponent from './components/ListEmployeeComponent'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
@@ -16,6 +17,8 @@ function App() {
         <Route path='/employees' element = {<ListEmployeeComponent/>}></Route>
         <Route path='/add-employee' element = {<EmployeeComponent/>}></Route>
         <Route path='/edit-employee/:id' element = {<EmployeeComponent/>}></Route>
+        {/* Department Routes */}
+        <Route path='/departments' element = {<ListDepartmentComponent/>}></Route>
       </Routes>
       <FooterComponent/>
     </BrowserRouter>
